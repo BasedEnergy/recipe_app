1bf15ba1fe18b6bb15092d4bade45b5f89e97104
basedenergy <hollindgolden@gmailcom>
2019-01-03 00:33:40 -0500

    deployed app

---
+++ build/precache-manifest.637a344c4562bc9eea95078959728592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5515967291883de90323",
+    "url": "/recipe_app/static/js/runtime~main.55159672.js"
+  },
+  {
+    "revision": "7474700ccff989fe5dc3",
+    "url": "/recipe_app/static/js/main.7474700c.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipe_app/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "7474700ccff989fe5dc3",
+    "url": "/recipe_app/static/css/main.34fcb20f.chunk.css"
+  },
+  {
+    "revision": "178a0029851f32347d11f2a66e061f88",
+    "url": "/recipe_app/index.html"
+  }
+];
